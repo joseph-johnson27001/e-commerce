3ee1e6597e8c9d9d2bced3df6fecf3dd4907ac04
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-08-26 08:26:58 +0700

    Shopping cart favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "e-commerce",
+    title: "E-Commerce",
     htmlAttrs: {
       lang: "en",
     },
@@ -17,7 +17,7 @@ export default {
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/shopping-cart.png" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
