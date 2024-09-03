13eeaffbc87b03bab41f7234768c633548c175c9
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-09-03 08:53:30 +0700

    Header icon update

--- components/Header.vue
+++ components/Header.vue
@@ -2,24 +2,48 @@
   <header class="text-black p-4">
     <div class="nav-container mx-auto flex justify-center">
       <!-- Navigation Links -->
-      <nav class="flex space-x-4">
-        <nuxt-link to="/" class="px-3 py-2 rounded text-sm"> Home </nuxt-link>
-        <nuxt-link to="/shop" class="px-3 py-2 rounded text-sm">
+      <nav class="flex space-x-4 items-center">
+        <nuxt-link to="/" class="flex items-center px-3 py-2 rounded">
+          <img
+            src="/images/shoe-icon.png"
+            alt="Home"
+            class="w-6 h-6 object-contain"
+          />
+        </nuxt-link>
+        <nuxt-link
+          to="/shop"
+          class="flex items-center px-3 py-2 rounded text-sm"
+        >
           Shop
         </nuxt-link>
-        <nuxt-link to="/new-arrivals" class="px-3 py-2 rounded text-sm">
+        <nuxt-link
+          to="/new-arrivals"
+          class="flex items-center px-3 py-2 rounded text-sm"
+        >
           New Arrivals
         </nuxt-link>
-        <nuxt-link to="/best-sellers" class="px-3 py-2 rounded text-sm">
+        <nuxt-link
+          to="/best-sellers"
+          class="flex items-center px-3 py-2 rounded text-sm"
+        >
           Best Sellers
         </nuxt-link>
-        <nuxt-link to="/sales" class="px-3 py-2 rounded text-sm">
+        <nuxt-link
+          to="/sales"
+          class="flex items-center px-3 py-2 rounded text-sm"
+        >
           Sales
         </nuxt-link>
-        <nuxt-link to="/about" class="px-3 py-2 rounded text-sm">
+        <nuxt-link
+          to="/about"
+          class="flex items-center px-3 py-2 rounded text-sm"
+        >
           About
         </nuxt-link>
-        <nuxt-link to="/contact-us" class="px-3 py-2 rounded text-sm">
+        <nuxt-link
+          to="/contact-us"
+          class="flex items-center px-3 py-2 rounded text-sm"
+        >
           Contact Us
         </nuxt-link>
       </nav>
@@ -39,5 +63,10 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+nav img {
+  display: block;
+  margin-bottom: 5px;
 }
 </style>
