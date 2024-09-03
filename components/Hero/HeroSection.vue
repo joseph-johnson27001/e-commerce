2ef8f0539df824240b5502de9bf340e963b760fe
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-09-03 08:47:23 +0700

    Updated hero section

--- components/Hero/HeroSection.vue
+++ components/Hero/HeroSection.vue
@@ -1,13 +1,13 @@
 <template>
   <section
-    class="hero-section bg-cover bg-center h-96 flex items-center justify-center text-center text-black"
+    class="hero-section bg-cover bg-center h-64 flex items-center justify-center text-center text-black"
   >
     <div class="hero-content max-w-xl">
       <h1 class="text-4xl md:text-6xl font-bold mb-4">
-        <span class="text-black"> Welcome </span> to Our Store
+        <span class="text-black"> Step Up </span> Your Game
       </h1>
       <p class="text-lg md:text-xl mb-6">
-        Discover the latest trends in fashion, tech, and more.
+        Find the perfect trainers to elevate your style and performance.
       </p>
     </div>
   </section>
