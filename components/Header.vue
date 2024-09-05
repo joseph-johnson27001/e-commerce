bc50feda73f89dc04ed64f1721651272c79a36f8
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-09-05 08:08:51 +0700

    Navigation on smaller vps

--- components/Header.vue
+++ components/Header.vue
@@ -1,21 +1,61 @@
 <template>
   <header class="text-black p-2">
-    <div class="nav-container mx-auto flex justify-center">
-      <!-- Navigation Links -->
-      <nav class="flex space-x-4 items-container">
-        <nuxt-link to="/" class="flex items-center px-3 py-2 rounded">
+    <!-- HIDE THIS SECTION ON SMALLER VIEWPORTS -->
+    <div class="nav-container mx-auto flex justify-between lg:justify-center">
+      <!-- Navigation Links - Visible on larger viewports -->
+      <nav class="hidden md:flex space-x-4 items-container">
+        <!-- Left Icon -->
+        <nuxt-link to="/" class="flex items-center px-3 py-2">
           <img
             src="/images/shoe-icon.png"
             alt="Home"
             class="w-6 h-6 object-contain"
           />
         </nuxt-link>
-
         <nuxt-link
           v-for="(link, index) in navLinks"
           :key="index"
           :to="link.url"
-          class="nav-link flex items-center px-3 py-2 rounded text-sm"
+          class="nav-link flex items-center px-3 py-2 text-sm"
+        >
+          {{ link.text }}
+        </nuxt-link>
+      </nav>
+
+      <!-- Burger Menu Button - Visible on smaller viewports -->
+      <nuxt-link to="/" class="md:hidden flex items-center px-3 py-2">
+        <img
+          src="/images/shoe-icon.png"
+          alt="Home"
+          class="w-6 h-6 object-contain"
+        />
+      </nuxt-link>
+      <button @click="toggleMenu" class="md:hidden px-3 py-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6h16.5M3.75 12h16.5M3.75 18h16.5"
+          />
+        </svg>
+      </button>
+    </div>
+
+    <!-- Dropdown for smaller viewports -->
+    <div v-if="menuOpen" class="md:hidden mt-2">
+      <nav class="flex flex-col space-y-2">
+        <nuxt-link
+          v-for="(link, index) in navLinks"
+          :key="index"
+          :to="link.url"
+          class="nav-link flex items-center px-4 py-2 text-sm"
         >
           {{ link.text }}
         </nuxt-link>
@@ -37,7 +77,13 @@ export default {
         { text: "Blog", url: "/" },
         { text: "FAQ", url: "/" },
       ],
+      menuOpen: false,
     };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen;
+    },
   },
 };
 </script>
@@ -46,8 +92,8 @@ export default {
 .nav-container {
   max-width: 1400px;
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: space-between;
   width: 100%;
 }
 
